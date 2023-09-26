2fe4da1868de68b4a7f6839e11fbe156eef61468
Guilherme Carvalho <guilherme.scrv@gmail.com>
2023-09-25 21:25:36 -0300

    update

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,5 @@ FROM golang:latest
 COPY . /app
 WORKDIR /app
 RUN go build -o main .
-EXPOSE 8080
-# Comando para iniciar a aplicação quando o contêiner for executado
+EXPOSE 3030
 CMD ["./main"]
--- main.go
+++ main.go
@@ -3,10 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"runtime"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -19,8 +17,8 @@ func sumEfficientHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	result, sum, memStats, cpuUsage := sumBenchmark(num, "efficientSum")
-	formattedResult := formatBenchmarkResult("efficientSum", num, result, sum, memStats, cpuUsage)
+	sum := efficientSum(num)
+	formattedResult := formatResult("efficientSum", num, sum)
 	fmt.Fprint(w, formattedResult)
 }
 
@@ -31,30 +29,9 @@ func sumInefficientHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid number", http.StatusBadRequest)
 		return
 	}
-
-	result, sum, memStats, cpuUsage := sumBenchmark(num, "inefficientSum")
-	formattedResult := formatBenchmarkResult("inefficientSum", num, result, sum, memStats, cpuUsage)
+	sum := inefficientSum(num)
+	formattedResult := formatResult("inefficientSum", num, sum)
 	fmt.Fprint(w, formattedResult)
-}
-
-func sumBenchmark(num int, function string) (time.Duration, int, runtime.MemStats, int) {
-	startCPU := runtime.NumCPU()
-	startTime := time.Now()
-	var memStats runtime.MemStats
-	sum := 0
-
-	if function == "efficientSum" {
-		sum = efficientSum(num)
-	} else {
-		sum = inefficientSum(num)
-	}
-
-	runtime.ReadMemStats(&memStats)
-	elapsedTime := time.Since(startTime)
-	endCPU := runtime.NumCPU()
-	cpuUsage := endCPU - startCPU
-
-	return elapsedTime, sum, memStats, cpuUsage
 }
 
 func efficientSum(n int) int {
@@ -70,27 +47,12 @@ func inefficientSum(n int) int {
 	return sum
 }
 
-func formatBenchmarkResult(name string, num int, elapsedTime time.Duration, sum int, memStats runtime.MemStats, cpuUsage int) string {
+func formatResult(name string, num int, sum int) string {
 	var builder strings.Builder
 	builder.WriteString(fmt.Sprintf("Function: %s\n", name))
 	builder.WriteString(fmt.Sprintf("N: %d\n", num))
 	builder.WriteString(fmt.Sprintf("Sum: %d\n", sum))
-	builder.WriteString(fmt.Sprintf("Time: %s\n", elapsedTime))
-	builder.WriteString(fmt.Sprintf("Memory Alloc: %s\n", formatBytes(memStats.Alloc)))
-	builder.WriteString(fmt.Sprintf("Memory Total Alloc: %s\n", formatBytes(memStats.TotalAlloc)))
-	builder.WriteString(fmt.Sprintf("Memory Heap Alloc: %s\n", formatBytes(memStats.HeapAlloc)))
-	builder.WriteString(fmt.Sprintf("CPU Usage: %d\n", cpuUsage))
 	return builder.String()
-}
-
-func formatBytes(bytes uint64) string {
-	const unit = 1024
-	if bytes < unit {
-		return fmt.Sprintf("%d B", bytes)
-	}
-	div := bytes / unit
-	mod := bytes % unit
-	return fmt.Sprintf("%.1f KiB", float64(div)+float64(mod)/float64(unit))
 }
 
 func main() {
@@ -98,6 +60,6 @@ func main() {
 	r.Get("/sum/efficient/{num}", sumEfficientHandler)
 	r.Get("/sum/inefficient/{num:[0-9]+}", sumInefficientHandler)
 
-	fmt.Println("HTTP server started on :8080")
-	http.ListenAndServe(":8080", r)
+	fmt.Println("HTTP server started on :3030")
+	http.ListenAndServe(":3030", r)
 }
